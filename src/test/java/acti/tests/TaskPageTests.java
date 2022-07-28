74c37679348869b1b1b91537fb604b2261bd3a19
eshadi2012 <shahrinsyeda@gmail.com>
2022-07-28 09:02:59 -0400

    TaskPageTwsts - added elements and methods for createing the customer

--- src/test/java/acti/tests/TaskPageTests.java
+++ src/test/java/acti/tests/TaskPageTests.java
@@ -1,14 +1,21 @@
 package acti.tests;
 
+import static org.testng.Assert.assertTrue;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TaskPageTests extends BaseTest{
 	
+	@Test
 	public void testCreateCustomer()
 	{
 	lp.enterUsername("admin");
 	lp.enterPassword("manager");
 	lp.clickLogin();
+	String expected="John Doe";
+	String actual = ep.validateUserLoggedIn();
+	Assert.assertEquals(actual, expected);
 	ep.clickTaskMenu();
 	tp.clickAddNew();
 	tp.clickNewCustomer();
@@ -16,7 +23,10 @@ public class TaskPageTests extends BaseTest{
 	tp.enterCustomerDiscription("t");
 	tp.clickCreateCustomer();
 	String successmessage = tp.getSucessMessage();
-	System.out.println(successmessage);
+	//System.out.println(successmessage);
+	Assert.assertTrue(successmessage.contains("has been created"));
+	ep.clickLogout();
+	
 		
 	}
 
