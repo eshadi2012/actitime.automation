db6cdac0c606514b1023bbf538f40930f9af5ff2
eshadi2012 <shahrinsyeda@gmail.com>
2022-07-30 11:11:49 -0400

    added sleep time to achieve synchronization in taskpage

--- src/main/java/acti/pages/TaskPage.java
+++ src/main/java/acti/pages/TaskPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import acti.driver.DriverManager;
+import acti.utils.Helper;
 
 public class TaskPage extends DriverManager{
 	
@@ -13,19 +14,63 @@ public class TaskPage extends DriverManager{
 	@FindBy(xpath="//div[@class='title ellipsis']") WebElement buttonAddNew;
 	@FindBy(xpath= "//div[@class='item createNewCustomer']") WebElement itemNewCustomer;
 	@FindBy(xpath="(//input[@placeholder='Enter Customer Name'])[2]") WebElement textCustomerName;
-	@FindBy(xpath="/textarea[@placeholder='Enter Customer Description']") WebElement textAreaCustomerDescription;
+	@FindBy(xpath="(//textarea[@placeholder='Enter Customer Description'])[1]") WebElement textAreaCustomerDescription;
 	@FindBy(xpath="//div[contains(text(),'Create Customer')]") WebElement buttonCreateCustomer;
 	@FindBy(xpath= "//span[@class='innerHtml']") WebElement TextSuccessMessage;
+	@FindBy(xpath="(//input[@placeholder='Start typing name ...'])[1]") WebElement textboxstarttyping;
+	@FindBy(xpath="//span[@class='highlightToken']") WebElement searchCustomer; 
+	@FindBy(xpath="//div[@class='titleEditButtonContainer']//div[@class='editButton']") WebElement buttonEdit;
+	@FindBy(xpath="//div[@class='editCustomerPanelHeader']//div[@class='action'][normalize-space()='ACTIONS']") WebElement buttonAction;
+	@FindBy(xpath="(//div[@class='deleteButton'])[1]") WebElement buttonDelete;
+	@FindBy(xpath="//span[normalize-space()='Delete permanently']") WebElement buttonDeletePermanatly;
 	
 
-//*****************************Page initialization************************
+//*****************************Page initialization**************************************
 		
+
 		public  TaskPage() 
 		{
 			PageFactory.initElements(driver, this);	
 		}
 
-//***************************** Page Action/ Method ************************
+//***************************** Page Action/ Method ***********************************
+		
+		public void clickDeletePermanatly()
+		{
+			buttonDeletePermanatly.click();
+			Helper.fn_sleep();
+			
+		}
+		public void clickDelete()
+		{
+			buttonDelete.click();
+		}
+		
+		public void clickAction()
+		{
+			buttonAction.click();
+			Helper.fn_sleep();
+		}
+		
+		public void clickEditButton()
+		{
+			buttonEdit.click();
+			Helper.fn_sleep();
+		}
+		
+		public void clicksearchCustomer()
+		{
+			searchCustomer.click();
+			Helper.fn_sleep();
+			
+		}
+		
+		public void enterCustomerType(String customername)
+		{
+			textboxstarttyping.sendKeys(customername);
+		
+		}
+		
 		public void clickAddNew() 
 		{
 			buttonAddNew.click();
@@ -44,19 +89,22 @@ public class TaskPage extends DriverManager{
 		public void enterCustomerDiscription(String customerdescription)
 		{
 			textAreaCustomerDescription.sendKeys(customerdescription);
+			Helper.fn_sleep();
 		}
 		public void clickCreateCustomer()
 		{
 			buttonCreateCustomer.click();
+			Helper.fn_sleep();
 		}
 		
 		public String getSucessMessage() 
 		{
 			return TextSuccessMessage.getText();	
 		}
-		public String getTaskPageTitle()
+		
+		/*public String getTaskPageTitle()
 		{
 			return driver.getTitle();
-		}
+		}*/
 
 }
